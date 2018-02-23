7cb46aab14a576a28283ae6ee73fb02a4293df47
21saavage <35587873+21saavage@users.noreply.github.com>
2018-02-23 10:14:00 +0530

    Add files via upload

---
+++ calculator _operation.py
@@ -0,0 +1,18 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+def flo(x,y):
+    return x//y
+def mod(x,y):
+    return x%y
+print('addition',add(5,55))
+print('subtraction',sub(5,55))
+print('multiply',mul(5,55))
+print('divison',div(5,55))
+print('float',flo(5,55))
+print('modulas',mod(5,55))
